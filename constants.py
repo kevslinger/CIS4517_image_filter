57370f6459de829d9f7d61d78eec1b8dbe145ac6
kevslinger <tug74186@temple.edu>
2020-03-24 17:21:18 -0400

    added constants file to keep track of paths, mainly

---
+++ constants.py
@@ -0,0 +1,5 @@
+BASE_FOLDER = '/Users/kevin/Desktop/New Classes/Cloud Computing/image_flask_app/'
+UPLOAD_FOLDER = 'uploads/'
+DOWNLOAD_FOLDER = 'downloads/'
+BUCKET = 'project1testbucket'
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
