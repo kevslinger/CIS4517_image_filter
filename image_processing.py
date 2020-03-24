187f017c840c92a651f45425610b2d1ecc33e98b
kevslinger <tug74186@temple.edu>
2020-03-24 17:21:29 -0400

    file for processing the different filters

---
+++ image_processing.py
@@ -0,0 +1,36 @@
+from PIL import Image, ImageOps,ImageFilter
+import constants
+
+
+def applyfilter(path, preset):
+    f = path.split('.')
+    outputfilename = f[0] + '-out-' + preset + '.' + f[1]
+    
+    im = Image.open(constants.UPLOAD_FOLDER + path)
+    if preset == 'gray':
+        im = ImageOps.gray
+
+    if preset == 'edge':
+        im = ImageOps.grayscale(im)
+        im = im.filter(ImageFilter.FIND_EDGES)
+
+    if preset == 'poster':
+        im = ImageOps.posterize(im, 3)
+
+    if preset == 'solar':
+        im = ImageOps.solarize(im, threshold=80)
+
+    if preset == 'blur':
+        im = im.filter(ImageFilter.BLUR)
+        
+    if preset == 'sepia':
+        sepia = []
+        r, g, b = (239, 224, 185)
+        for i in range(255):
+            sepia.extend((r*i/255, g*i/255, b*i/255))
+        im = im.convert("L")
+        im.putpalette(sepia)
+        im = im.convert("RGB")
+
+    im.save(constants.UPLOAD_FOLDER + outputfilename)
+    return outputfilename
