3d4708ee6a378ca63c217801170bd0a95e53ba15
kevslinger <tug74186@temple.edu>
2020-03-24 17:19:49 -0400

    updated s3 ops file

--- s3_operations.py
+++ s3_operations.py
@@ -14,3 +14,10 @@ def download_file(file_name, bucket):
 
     return output
 
+def list_files(bucket):
+    s3 = boto3.client('s3')
+    contents = []
+    for item in s3.list_objects(Bucket=bucket)['Contents']:
+        contents.append(item)
+
+    return contents
