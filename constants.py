546a2984eb30b8687de9fc58b4d62be2cc336ddb
kevslinger <tug74186@temple.edu>
2020-03-25 09:48:24 -0400

    moved utility functions here

--- constants.py
+++ constants.py
@@ -3,3 +3,16 @@ UPLOAD_FOLDER = 'static/'
 DOWNLOAD_FOLDER = 'downloads/'
 BUCKET = 'project1testbucket'
 ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# TODO: NOT NEEDED FOR REMOTE
+import glob
+def local_list_files():
+    file_list = []
+    for filename in glob.glob('/Users/kevin/Desktop/New Classes/Cloud Computing/image_flask_app/uploads/*.jpg'):
+        file_list.append({'key' : filename})
+    return file_list
+# END TODO
